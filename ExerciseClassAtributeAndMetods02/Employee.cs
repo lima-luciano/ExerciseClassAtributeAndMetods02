acc73395f6fff94e3a5bb59491b2dc1e2b9f99b6
Devllu <dev.limaluciano@gmail.com>
2021-12-11 10:15:15 -0300

    Add project files.

---
+++ ExerciseClassAtributeAndMetods02/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods02
+{
+    class Employee
+    {
+        public string Name;
+        public double Salary;
+        public double Tax;
+
+        public double TotalValueAfterTheft()
+        {
+            return Salary - Tax;
+        } 
+        
+        public void IncreaseSalary(double salary)
+        {
+            Salary += Salary *10 / 100;
+        }
+
+        public override string ToString()
+        {
+            return "Employee: " + Name + ", Will receive: R$ " + TotalValueAfterTheft().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExerciseClassAtributeAndMetods02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee f1 = new Employee(); 
+
+            Console.Write("Type your name: ");
+            f1.Name = Console.ReadLine();
+            Console.Write("groos salary: ");
+            f1.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: ");
+            f1.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write(f1);
+            Console.WriteLine();
+
+            Console.Write("Type porcentage to increase the salary: ");
+            double inc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f1.IncreaseSalary(inc);
+
+            Console.Write("Update data: " + f1);
+
+        }
+    }
+}
